1723c3e271020c30a46ef9253b6d353052a8a179
Tirsa St. Fort <lifelovelori@gmail.com>
2023-01-23 20:25:40 -0800

    Comoleted lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+
+function findMatching(array, driverName){
+    let result = array.filter(driver => driver.toLowerCase() === driverName.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(array, letters){
+    let result = array.filter(driver => driver.startsWith(letters))
+    return result
+}
+
+function matchName(array, name){
+    let result = array.filter(element => element.name === name)
+    return result
+}
